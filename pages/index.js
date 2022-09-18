c8e4d2bd5848748211eb01e3d1f57840ec0eae97
Ojo Patrick Duke <59659665+dukepat@users.noreply.github.com>
2022-09-18 20:11:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>OWNly App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
